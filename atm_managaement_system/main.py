61f84b5801e428e39fcbd34718d5608d1b2215b4
pallavireddy3439 <pallavireddy3439@gmail.com>
2026-01-11 09:49:47 +0530

    create  main.py

--- atm_managaement_system/main.py
+++ atm_managaement_system/main.py
@@ -4,37 +4,47 @@ from storage import load_accounts, save_accounts
 accounts = load_accounts()
 
 if not accounts:
-    accounts[122232] = Atm("ramu", 3699, 122232, "Andhra Bank")
+    print("No accounts found. Creating default account...")
+    acc = Atm("Ramu", 3699, 122232, "Andhra Bank", 1000)
+    accounts[acc.accno] = acc
+    save_accounts(accounts)
 
-pin = int(input("Enter PIN: "))
-acc = accounts[122232]
 
-if pin != acc.pin:
-    print("Incorrect PIN")
-else:
-    while True:
-        print("\n1.Account Details\n2.Deposit\n3.Withdraw\n4.Check Balance\n5.Exit")
-        choice = int(input("Enter your choice: "))
+accno = int(input("Enter account number: "))
 
-        if choice == 1:
-            acc.details()
+if accno not in accounts:
+    print("Account not found")
+    exit()
 
-        elif choice == 2:
-            amount = int(input("Enter deposit amount: "))
-            acc.deposit(amount)
-            save_accounts(accounts)
+atm = accounts[accno]
 
-        elif choice == 3:
-            amount = int(input("Enter withdraw amount: "))
-            acc.withdraw(amount)
-            save_accounts(accounts)
+pin = int(input("Enter pin: "))
+if pin != atm.pin:
+    print("Incorrect pin")
+    exit()
+    
+while True:
+    print("\n1.Account details\n2.Deposit\n3.Withdraw\n4.Check Balance\n5.Exit")
+    ch = int(input("Enter your choice: "))
 
-        elif choice == 4:
-            acc.check_balance()
+    if ch == 1:
+        atm.details()
 
-        elif choice == 5:
-            save_accounts(accounts)
-            break
+    elif ch == 2:
+        atm.deposit()
+        save_accounts(accounts)
 
-        else:
-            print("Invalid choice")
+    elif ch == 3:
+        atm.withdraw()
+        save_accounts(accounts)
+
+    elif ch == 4:
+        atm.checkbalance()
+
+    elif ch == 5:
+        save_accounts(accounts)
+        print("Thank you for using ATM")
+        break
+
+    else:
+        print("Invalid choice")
